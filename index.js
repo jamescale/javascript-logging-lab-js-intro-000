632e825441bd9c32f8fca09ea09e435e65f4be72
James Cale <jamesdcale@gmail.com>
2019-08-13 19:47:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-//declare the variable
-var word
-
-//assign a value to the variable
-word = 'bird'
-
-console.log(bird) //'bird'
-
-//assign another value to the variable
-
-word = 'dog'
-
-console.log(dog) //'dog'
-
 var word = 'bird'
 
 window.word
